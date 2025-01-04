d661596ce4d350d00c0fca8a0448892f7c173b93
VladimirLezhnin <115662980+VladimirLezhnin@users.noreply.github.com>
2025-01-05 00:06:32 +0500

    fix

--- src/components/offers-list/offer-card/offer-card.tsx
+++ src/components/offers-list/offer-card/offer-card.tsx
@@ -31,7 +31,7 @@ const GetStyleParameters = (pathName: string) => {
     imageHeight: '200'
   };
 
-  switch(pathName) {
+  switch (pathName) {
     case `${AppRoute.Root}`:
       return styleParametersForRootPage;
     case `${AppRoute.Favorites}`:
@@ -41,7 +41,7 @@ const GetStyleParameters = (pathName: string) => {
   }
 };
 
-function OfferCard({offer: offer, onMouseOver: onMouseOver}: OfferCardProps) {
+function OfferCard({ offer: offer, onMouseOver: onMouseOver }: OfferCardProps) {
   const location = useLocation();
   const { id, price, previewImage, title, rating, isPremium, type } = offer;
   const { block, imageWidth, imageHeight } = GetStyleParameters(location.pathname);
@@ -71,52 +71,49 @@ function OfferCard({offer: offer, onMouseOver: onMouseOver}: OfferCardProps) {
   };
 
   return (
-    <div
+    <article className={`${block}__card place-card`}
       onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <article className={`${block}__card place-card`}>
-        {
-          isPremium ?
-            <div className="place-card__mark">
-              <span>Premium</span>
-            </div> : <div></div>
-        }
-        <div className={`${block}__image-wrapper place-card__image-wrapper`}>
-          <Link to={`/offer/${id}`}>
-            <img className="place-card__image" src={`${previewImage}`} width={imageWidth} height={imageHeight} alt="Place image"/>
-          </Link>
-        </div>
-        <div className="place-card__info">
-          <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-              <b className="place-card__price-value">&euro;{price}</b>
-              <span className="place-card__price-text">&#47;&nbsp;night</span>
-            </div>
-            <button
-              className={`place-card__bookmark-button button ${isFavorite ? 'place-card__bookmark-button--active' : ''}`}
-              type="button"
-              onClick={handleBookmarkClick}
-            >
-              <svg className="place-card__bookmark-icon" width="18" height="19">
-                <use xlinkHref="#icon-bookmark"></use>
-              </svg>
-              <span className="visually-hidden">To bookmarks</span>
-            </button>
+      onMouseLeave={handleMouseLeave}>
+      {
+        isPremium ?
+          <div className="place-card__mark">
+            <span>Premium</span>
+          </div> : <div></div>
+      }
+      <div className={`${block}__image-wrapper place-card__image-wrapper`}>
+        <Link to={`/offer/${id}`}>
+          <img className="place-card__image" src={`${previewImage}`} width={imageWidth} height={imageHeight} alt="Place image" />
+        </Link>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-              <span style={{width: `${20 * roundNumberWithTieBreak(rating)}%`}}></span>
-              <span className="visually-hidden">Rating</span>
-            </div>
-          </div>
-          <h2 className="place-card__name">
-            <Link to={`/offer/${id}`}>{title}</Link>
-          </h2>
-          <p className="place-card__type">{capitalizeFirstLetter(type)}</p>
+          <button
+            className={`place-card__bookmark-button button ${isFavorite ? 'place-card__bookmark-button--active' : ''}`}
+            type="button"
+            onClick={handleBookmarkClick}
+          >
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
         </div>
-      </article>
-    </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{ width: `${20 * roundNumberWithTieBreak(rating)}%` }}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <Link to={`/offer/${id}`}>{title}</Link>
+        </h2>
+        <p className="place-card__type">{capitalizeFirstLetter(type)}</p>
+      </div>
+    </article>
   );
 }
 
